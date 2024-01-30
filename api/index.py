7f752868f7dc43a2ec084834cafe853a5546ad91
pythonthm <pythonthm@gmail.com>
2024-01-31 08:13:31 +0900

    delete-methods

--- api/index.py
+++ api/index.py
@@ -7,20 +7,22 @@ app = Flask(__name__)
 def home():
     return render_template('index.html',title="嫌難しいな",message="本当にね")
 
-@app.route('/')
-def post():
-	name = request.form['name']
+# @app.route('/',methods=['POST'])
+# def post():
+# 	name = request.form['name']
 	
-	shortened_link = Shortener().tinyurl.short(name)
-	print(shortened_link)
-	return render_template('index.html', \
-		title = 'Form Sample(post)', \
-		message = '短縮urlは、{}'.format(shortened_link))
+# 	shortened_link = Shortener().tinyurl.short(name)
+# 	print(shortened_link)
+# 	return render_template('index.html', \
+# 		title = 'Form Sample(post)', \
+# 		message = '短縮urlは、{}'.format(shortened_link))
 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
 
 
 

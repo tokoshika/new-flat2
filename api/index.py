36eba6300401691caff3d5c822e384945d078c4e
pythonthm <pythonthm@gmail.com>
2024-01-30 16:14:30 +0900

    add-about

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html',title="嫌難しいな",message="本当にね")
 
 @app.route('/about')
 def about():

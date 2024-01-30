5887d9c1c556833ceeb59366fa8a0d08e8f30248
pythonthm <pythonthm@gmail.com>
2024-01-30 15:20:42 +0900

    second

--- api/index.py
+++ api/index.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!ssssPPP'
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
     return 'About2'
 
-app.run(port=5000,debug=True)
+# app.run(port=5000,debug=True)
